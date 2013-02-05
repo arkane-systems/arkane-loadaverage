469a6e178a773e4aec9d7a57f8abe92f19cbe1a8
Alistair Young <avatar@arkane-systems.net>
2013-02-05 08:58:34 -0600

    Initial template files.

---
+++ LoadAverage/LoadAvg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadAverage
+{
+    public partial class LoadAvg : ServiceBase
+    {
+        public LoadAvg()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
